d74aa800bb57f08c601930e588072ef90523bd68
Vikranth94 <vikranth.1994@gmail.com>
2017-05-14 20:24:08 -0700

    part d

---
+++ HW4/contour_plot.m
@@ -0,0 +1,8 @@
+function contour_plot(mu,Sigma,colour)
+    x1 = 0:.2:20; x2 = 0:.2:20;
+    [X1,X2] = meshgrid(x1,x2);
+    F = mvnpdf([X1(:) X2(:)],mu,Sigma);
+    F = reshape(F,length(x2),length(x1));
+    hold on;
+    contour(X1,X2,F,[0.007 0.007],colour)
+end
